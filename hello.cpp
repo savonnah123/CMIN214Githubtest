9f4748c23d37436fc533d941d432f959330c5317
savonnah123 <116669177+savonnah123@users.noreply.github.com>
2022-10-25 13:48:41 -0500

    github code
    cmin test

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+/*
+Author: Savonnah Watts
+Date: 10/25/2022
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+
+{ 
+    cout<<"Hello world my name is Savonnah. This is a test." << endl;
+    return 0;
+}
